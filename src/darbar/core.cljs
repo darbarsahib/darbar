991ae1d400ca0bd6490a00eb5ba7903877f2550b
Arjan Singh <arjansingh1989@gmail.com>
2017-07-04 13:41:31 -0700

    Clean up.

--- src/darbar/core.cljs
+++ src/darbar/core.cljs
@@ -3,8 +3,6 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/darbar/core.cljs. Go ahead and edit it and see reloading in action.")
-
 ;;
 ;; Definitions
 ;;
@@ -62,7 +60,7 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+; (defonce app-state (atom {:text "Hello world!"}))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
