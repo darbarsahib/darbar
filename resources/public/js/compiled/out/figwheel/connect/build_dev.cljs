8429193fd5ea5b099a19358c39c3429670c1f15d
Arjan Singh <arjansingh1989@gmail.com>
2017-07-04 23:00:11 -0700

    Minified version of js for production.

--- resources/public/js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [darbar.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/darbar.core.on-js-reload (apply js/darbar.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'darbar.core/on-js-reload' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
